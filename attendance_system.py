7427f551ea1f760580e34b90ed89b75d53beaf67
SusanGautam77 <131547139+SusanGautam77@users.noreply.github.com>
2023-10-03 10:55:20 +0545

    attendance_system.py

--- attendance_system.py
+++ attendance_system.py
@@ -1,45 +1,80 @@
 import RPi.GPIO as GPIO
 import time
-from mfrc522 import SimpleMFRC522
 import board
 import busio
-import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+import adafruit_ssd1306
+import mfrc522
+import datetime
 
-# Initialize LCD
-lcd_columns = 16
-lcd_rows = 2
+# Connect the RFID reader to the Raspberry Pi
+GPIO.setmode(GPIO.BCM)
+
+# RFID reader pins
+RFID_SDA = 24
+RFID_SCK = 23
+RFID_MOSI = 19
+RFID_MISO = 21
+RFID_RST = 22
+RFID_3V3 = 1
+
+# Set up the GPIO pins for the RFID reader
+GPIO.setup(RFID_SDA, GPIO.IN)
+GPIO.setup(RFID_SCK, GPIO.OUT)
+GPIO.setup(RFID_MOSI, GPIO.OUT)
+GPIO.setup(RFID_MISO, GPIO.IN)
+GPIO.setup(RFID_RST, GPIO.OUT)
+GPIO.setup(RFID_3V3, GPIO.OUT)
+
+# Connect the LCD display to the Raspberry Pi
 i2c = busio.I2C(board.SCL, board.SDA)
-lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 
-# Initialize RFID Reader
-reader = SimpleMFRC522()
+# Create an SSD1306 object for the LCD display
+display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 
-try:
-    while True:
-        # Clear LCD
-        lcd.clear()
+# Create a new MFRC522 object
+rc522 = mfrc522.MFRC522()
 
-        # Display message on LCD
-        lcd.message = "Place your card\non the reader."
+# Connect to the database
+# ...
 
-        # Wait for card
-        id, text = reader.read()
+# Define a function to check if a card is registered in the database
+def is_card_registered(uid):
+    # Check if the card is registered in the database
+    # ...
 
-        # Clear LCD
-        lcd.clear()
+    # Return True if the card is registered, False otherwise
+    return ...
 
-        # Display attendance message
-        lcd.message = f"ID: {id}\nAttendance recorded."
+# Define a function to update the attendance record for a user
+def update_attendance(uid):
+    # Update the attendance record for the user associated with the card
+    # ...
 
-        # Print ID to console
-        print(f"ID: {id}")
+# Define a function to display the attendance report
+def display_attendance_report():
+    # Get the attendance report for all users
+    # ...
 
-        # Add your attendance handling logic here
+    # Display the attendance report on the LCD display
+    # ...
 
-        # Pause for a moment
-        time.sleep(2)
+# Start the main loop
+while True:
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    lcd.clear()
-    lcd.color = [0, 0, 0]  # Turn off LCD backlight
+    # Read an RFID card
+    uid = read_rfid_card()
+
+    # If a card is read, check if it is registered in the database
+    if uid:
+        if is_card_registered(uid):
+            # Update the attendance record for the user
+            update_attendance(uid)
+        else:
+            # Display an error message on the LCD display
+            display.clear()
+            display.text('Card not registered', 0, 0, 1)
+            display.show()
+            time.sleep(2)
+
+    # Wait for 1 second
+    time.sleep(1)
